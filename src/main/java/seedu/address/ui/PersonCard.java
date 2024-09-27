6d235d4aaa85f4bcbd756c8cf0af285a05635095
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-09-28 00:19:28 +0800

    Add Remark Command

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -14,6 +14,9 @@ import seedu.address.model.person.Person;
  */
 public class PersonCard extends UiPart<Region> {
 
+    @FXML
+    private Label remark;
+
     private static final String FXML = "PersonListCard.fxml";
 
     /**
@@ -55,5 +58,6 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        remark.setText(person.getRemark().value);
     }
 }
